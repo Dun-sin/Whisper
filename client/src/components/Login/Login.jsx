d9ee2ca8dd3f3114c73c5d9ba580a539a925df44
Dunsin <jesudunsinfaiye@gmail.com>
2022-06-02 14:31:37 +0100

    added sawo integration

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import axios from 'axios';
+import Sawo from "sawo"
 
 // redux
 import { useSelector } from 'react-redux';
@@ -9,54 +10,62 @@ import { useDispatch } from 'react-redux';
 const centerStuffs = `flex flex-col justify-center items-center`
 
 const baseUrl = 'http://localhost:4000/user';
+const apiKey = process.env.REACT_APP_IMPORTANT;
 
 const date = new Date()
 let userID = Math.floor(+`${date.getTime()}${date.getDate()}${date.getMonth()}` / 10000);
 
 const Login = () => {
-  const emailRef = useRef();
   const dispatch = useDispatch();
 
-  const checkingUser = () => {
-    const email = emailRef.current.value;
+  useEffect(() => {
+    var config = {
+      containerID: "sawo-container",
+      identifierType: "email",
+      apiKey: '' + apiKey,
+      onSuccess: (payload) => {
+        checkingUser(payload.identifier)
+      },
+    };
+    let sawo = new Sawo(config)
+    sawo.showForm()
 
-    axios.get(`${baseUrl}/find?email=${email}`)
-      .then(res => {
-        console.log(res.status)
-        if (res.status === 202) {
-          loginUser(userID, email)
-        } else {
-          dispatch(addID(res.data.id))
-        }
-      })
-      .catch(err => console.log(err))
-  }
 
-  function loginUser(id, email) {
-    axios.post(`${baseUrl}/add`, {
-      id: id,
-      email: email
-    })
-      .then(res => {
-        if (res.status === 202) {
-          console.log('done')
-        } else {
-          console.log('failed')
-        }
+
+    const checkingUser = (email) => {
+      axios.get(`${baseUrl}/find?email=${email}`)
+        .then(res => {
+          console.log(res.status)
+          if (res.status === 202) {
+            loginUser(userID, email)
+          } else {
+            dispatch(addID(res.data.id))
+          }
+        })
+        .catch(err => console.log(err))
+    }
+
+    function loginUser(id, email) {
+      axios.post(`${baseUrl}/add`, {
+        id: id,
+        email: email
       })
-      .catch(err => console.log(err))
-  }
+        .then(res => {
+          if (res.status === 202) {
+            console.log('done')
+          } else {
+            console.log('failed')
+          }
+        })
+        .catch(err => console.log(err))
+    }
+  }, [])
 
   return (
     <div className={`bg-primary h-[100vh] w-[100vw] text-white ${centerStuffs}`}>
-      <h1 className='text-[2em] font-semibold mb-[10px]'>Sign-in</h1>
-      <div className={`${centerStuffs}`}>
-        <input type="email" name="email" placeholder='Enter your email....' className='h-[30px] w-[290px] p-[20px] rounded-[10px] text-black' ref={emailRef} />
-        <div className="flex items-center justify-between  w-[290px]">
-          <button type="submit" className={`mt-[10px] pb-[5px] bg-[#FF9F1C] text-primary text-white h-[30px] w-[120px] rounded-[10px]`} onClick={checkingUser}>Login</button>
-          <a className='text-[11.5px] mt-[15px]' href='/searchuser'>Login Anonymously</a>
-        </div>
-      </div>
+      <h1 className='text-[2em] font-semibold '>Sign-in</h1>
+      <div id="sawo-container" style={{ minHeight: "235px", width: "400px" }}></div>
+      <a className={`pb-[5px] bg-secondary text-white h-[30px] w-[370px] h-[50px] rounded-[10px] font-light ${centerStuffs}`} href='/searchuser'>Login Anonymously</a>
     </div>
   )
 }
