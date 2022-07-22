edc235fab17b8e3097bfa2f8e845995bc1cba7e4
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-07-22 02:54:35 +0000

    chore: reformat code to make eslint happy

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import { SocketContext } from '../../Context';
 
-import './Chat.css'
+import './Chat.css';
 
 import ScrollToBottom from 'react-scroll-to-bottom';
 import { IoSend } from 'react-icons/io5';
@@ -11,90 +11,116 @@ import { addMessages } from '../../redux/Reducers/messageSlice';
 
 let senderId;
 const Chat = () => {
-  const socket = useContext(SocketContext)
-  const [sentMessages, setSentMessages] = useState([]);
-  const [receivedMessages, setReceivedMessages] = useState([]);
-  const [messages, setMessages] = useState([]);
-  const inputRef = useRef('');
+    const socket = useContext(SocketContext);
+    const [sentMessages, setSentMessages] = useState([]);
+    const [receivedMessages, setReceivedMessages] = useState([]);
+    const [messages, setMessages] = useState([]);
+    const inputRef = useRef('');
 
-  const gettingState = useSelector(state => state);
-  const state = gettingState.messages;
-  senderId = gettingState.ID;
-  const dispatch = useDispatch();
+    const gettingState = useSelector((state) => state);
+    const state = gettingState.messages;
+    senderId = gettingState.ID;
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    socket.on('receive_message', ({ senderId, message, time }) => {
-      dispatch(addMessages({
-        id: senderId,
-        messages: {
-          message: message,
-          time: time
-        },
-        room: 'anon'
-      }))
-    })
-  }, [dispatch, socket])
+    useEffect(() => {
+        socket.on('receive_message', ({ senderId, message, time }) => {
+            dispatch(
+                addMessages({
+                    id: senderId,
+                    messages: {
+                        message: message,
+                        time: time,
+                    },
+                    room: 'anon',
+                })
+            );
+        });
+    }, [dispatch, socket]);
 
+    useEffect(() => {
+        const userIDs = Object.keys(state).map((item) => Number(item));
+        const available = userIDs.length === 0;
+        const sendID = userIDs.find((item) => item === senderId);
+        const receiverID = userIDs.find((item) => item !== senderId);
+        const handleMessages = () => {
+            sendID && setSentMessages(state[sendID].messages);
+            receiverID && setReceivedMessages(state[receiverID].messages);
+        };
+        !available && handleMessages();
+    }, [state]);
 
-  useEffect(() => {
-    const userIDs = Object.keys(state).map(item => +item);
-    const available = userIDs.length === 0;
-    const sendID = userIDs.find(item => item === senderId);
-    const receiverID = userIDs.find(item => item !== senderId);
-    const handleMessages = () => {
-      sendID && setSentMessages(state[sendID].messages);
-      receiverID && setReceivedMessages(state[receiverID].messages)
-    }
-    !available && handleMessages();
-  }, [state])
+    useEffect(() => {
+        const array = [...sentMessages, ...receivedMessages].sort((a, b) => {
+            const da = new Date(a.time),
+                db = new Date(b.time);
+            return da - db;
+        });
+        setMessages(array);
+    }, [sentMessages, receivedMessages]);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const d = new Date();
+        const time = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+        const message = inputRef.current.value;
+        if (message === '' || senderId === undefined || senderId === '123456') {
+            return;
+        }
+        socket.emit('send_message', { senderId, message, time });
+        dispatch(
+            addMessages({
+                id: senderId,
+                messages: {
+                    message: message,
+                    time: time,
+                },
+                room: 'anon',
+            })
+        );
+        inputRef.current.value = '';
+    };
 
-  useEffect(() => {
-    const array = [...sentMessages, ...receivedMessages].sort((a, b) => {
-      let da = new Date(a.time),
-        db = new Date(b.time);
-      return da - db;
-    });
-    setMessages(array)
-  }, [sentMessages, receivedMessages])
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    const d = new Date();
-    const time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    const message = inputRef.current.value;
-    if (message === '' || senderId === undefined || senderId === '123456') return
-    socket.emit('send_message', { senderId, message, time });
-    dispatch(addMessages({
-      id: senderId,
-      messages: {
-        message: message,
-        time: time
-      },
-      room: 'anon'
-    }))
-    inputRef.current.value = '';
-  }
-
-  return (
-    <div className='w-[100%] h-[90%] pb-[25px]'>
-      <p className='text-[0.8em] font-semibold mb-[20px] text-center'>Connected with a random User</p>
-      <ScrollToBottom initialScrollBehavior='auto' className="displayMessgaes h-[75%] w-[100%]">
-        {messages.map(({ message, time }, index) => (
-          <div key={index} className={`relative mb-[15px] w-[250px] text-primary ml-auto}`}>
-            <p className='bg-[#FF9F1C] rounded-[20px] p-[15px] break-all'>{message}</p>
-            <p className='text-white ml-[75%] text-[12px]'>{time}</p>
-          </div>
-        ))}
-      </ScrollToBottom>
-      <form className='flex justify-center items-center mt-[40px]' onSubmit={handleSubmit}>
-        <input placeholder='Send a Message.....' className='h-[65px] focus:outline-none rounded-[15px] bg-secondary w-[100%] text-white pl-[22px] pr-[22px] text-[18px]' ref={inputRef} />
-        <button type="submit" className='bg-[#FF9F1C] h-[65px] w-[70px] flex justify-center items-center rounded-[10px]'>
-          <IoSend className='fill-primary scale-[2]' />
-        </button>
-      </form>
-    </div>
-  )
-}
+    return (
+        <div className="w-[100%] h-[90%] pb-[25px]">
+            <p className="text-[0.8em] font-semibold mb-[20px] text-center">
+                Connected with a random User
+            </p>
+            <ScrollToBottom
+                initialScrollBehavior="auto"
+                className="displayMessgaes h-[75%] w-[100%]"
+            >
+                {messages.map(({ message, time }, index) => (
+                    <div
+                        key={index}
+                        className={`relative mb-[15px] w-[250px] text-primary ml-auto}`}
+                    >
+                        <p className="bg-[#FF9F1C] rounded-[20px] p-[15px] break-all">
+                            {message}
+                        </p>
+                        <p className="text-white ml-[75%] text-[12px]">
+                            {time}
+                        </p>
+                    </div>
+                ))}
+            </ScrollToBottom>
+            <form
+                className="flex justify-center items-center mt-[40px]"
+                onSubmit={handleSubmit}
+            >
+                <input
+                    placeholder="Send a Message....."
+                    className="h-[65px] focus:outline-none rounded-[15px] bg-secondary w-[100%] text-white pl-[22px] pr-[22px] text-[18px]"
+                    ref={inputRef}
+                />
+                <button
+                    type="submit"
+                    className="bg-[#FF9F1C] h-[65px] w-[70px] flex justify-center items-center rounded-[10px]"
+                >
+                    <IoSend className="fill-primary scale-[2]" />
+                </button>
+            </form>
+        </div>
+    );
+};
 
 export default Chat;
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import MojoAuth from 'mojoauth-web-sdk';
 
-// redux
+// Redux
 import { changeIsLogged } from '../../redux/Reducers/isLogged';
 import { addID } from '../../redux/Reducers/idSlice';
 import { useDispatch } from 'react-redux';
@@ -15,120 +15,135 @@ const apiKey = import.meta.env.REACT_APP_IMPORTANT;
 let userID = '';
 
 const Login = () => {
-	const dispatch = useDispatch();
-	const [loginStatus, setLoginStatus] = useState({
-		email: null,
-		status: 'none',
-		error: false,
-	});
-
-  useEffect(() => {
-    // Regenerate userId on each page load/route change
-    // This prevents us from re-using the same userId twice
-    userID = Math.random().toFixed(12).toString(36).slice(2);
-
-    const mojoauth = new MojoAuth(`${apiKey}`, {
-      source: [{ type: 'email', feature: 'magiclink' }]
+    const dispatch = useDispatch();
+    const [loginStatus, setLoginStatus] = useState({
+        email: null,
+        status: 'none',
+        error: false,
     });
-    mojoauth.signIn().then(payload => {
-      checkingUser(payload.user.email)
-      document.getElementById("mojoauth-passwordless-form").remove();
-    })
-  }, [])
 
-  useEffect(() => {
-    if (loginStatus.status === 'complete' && !loginStatus.error) {
-      dispatch(changeIsLogged({
-        isLoggedIn: true,
-        loginType: loginStatus.email ? 'email' : 'anonymous',
-        loginId: userID,
-        email: loginStatus.email,
-      }))
+    function loginUser(email) {
+        setLoginStatus({
+            ...loginStatus,
+            status: 'loading',
+            error: false,
+        });
+
+        axios
+            .post(`${baseUrl}/add`, {
+                email: email,
+            })
+            .then((res) => {
+                if (res.status === 202) {
+                    console.log('done');
+                    setLoginStatus({
+                        ...loginStatus,
+                        status: 'complete',
+                        error: false,
+                        email,
+                    });
+                } else {
+                    console.log('failed');
+                    setLoginStatus({
+                        ...loginStatus,
+                        status: 'complete',
+                        error: true,
+                    });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoginStatus({
+                    ...loginStatus,
+                    status: 'complete',
+                    error: true,
+                });
+            });
     }
-  }, [loginStatus, dispatch])
 
-	const checkingUser = email => {
-		axios
-			.get(`${baseUrl}/find?email=${email}`)
-			.then(res => {
-				if (res.status === 202) {
-					loginUser(email);
-					function loginUser(email) {
-						setLoginStatus({
-							...loginStatus,
-							status: 'loading',
-							error: false,
-						});
+    const checkingUser = (email) => {
+        axios
+            .get(`${baseUrl}/find?email=${email}`)
+            .then((res) => {
+                if (res.status === 202) {
+                    loginUser(email);
+                } else if (res.status === 200) {
+                    setLoginStatus({
+                        ...loginStatus,
+                        status: 'complete',
+                        error: false,
+                        email,
+                    });
+                    dispatch(addID(res.data.id));
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoginStatus({
+                    ...loginStatus,
+                    status: 'complete',
+                    error: true,
+                });
+            });
+    };
 
-						axios
-							.post(`${baseUrl}/add`, {
-								email: email,
-							})
-							.then(res => {
-								if (res.status === 202) {
-									console.log('done');
-									setLoginStatus({
-										...loginStatus,
-										status: 'complete',
-										error: false,
-										email,
-									});
-								} else {
-									console.log('failed');
-									setLoginStatus({
-										...loginStatus,
-										status: 'complete',
-										error: true,
-									});
-								}
-							})
-							.catch(err => {
-								console.log(err);
-								setLoginStatus({
-									...loginStatus,
-									status: 'complete',
-									error: true,
-								});
-							});
-					}
-				} else if (res.status === 200) {
-					setLoginStatus({
-						...loginStatus,
-						status: 'complete',
-						error: false,
-						email,
-					});
-					dispatch(addID(res.data.id));
-				}
-			})
-			.catch(err => {
-				console.log(err);
-				setLoginStatus({
-					...loginStatus,
-					status: 'complete',
-					error: true,
-				});
-			});
-	};
+    useEffect(() => {
+        // Regenerate userId on each page load/route change
+        // This prevents us from re-using the same userId twice
+        userID = Math.random().toFixed(12).toString(36).slice(2);
 
-  const loginAnonymously = () => {
-    setLoginStatus({
-      status: 'complete',
-      error: false,
-      email: null,
-    })
-  }
+        const mojoauth = new MojoAuth(`${apiKey}`, {
+            source: [{ type: 'email', feature: 'magiclink' }],
+        });
+        mojoauth.signIn().then((payload) => {
+            checkingUser(payload.user.email);
+            document.getElementById('mojoauth-passwordless-form').remove();
+        });
+    }, []);
 
-  return (
-    <div className={`bg-primary h-[100vh] w-[100vw] text-white ${centerStuffs}`}>
-      <h1 className='text-[2em] font-semibold'>Sign-in</h1>
-      {loginStatus.status !== 'loading' && <div className="pb-3">
-        <div id="mojoauth-passwordless-form" className=""></div>
-      </div>}
-      {loginStatus.status === 'loading' && <div className="uppercase py-5">Processing Login</div>}
-      <button disabled={loginStatus.status === 'loading'} onClick={loginAnonymously} className={`disabled:bg-slate-700 pb-[5px] bg-secondary text-white w-[370px] h-[50px] rounded-[10px] font-light ${centerStuffs}`}>Login Anonymously</button>
-    </div>
-  )
-}
+    useEffect(() => {
+        if (loginStatus.status === 'complete' && !loginStatus.error) {
+            dispatch(
+                changeIsLogged({
+                    isLoggedIn: true,
+                    loginType: loginStatus.email ? 'email' : 'anonymous',
+                    loginId: userID,
+                    email: loginStatus.email,
+                })
+            );
+        }
+    }, [loginStatus, dispatch]);
+
+    const loginAnonymously = () => {
+        setLoginStatus({
+            status: 'complete',
+            error: false,
+            email: null,
+        });
+    };
+
+    return (
+        <div
+            className={`bg-primary h-[100vh] w-[100vw] text-white ${centerStuffs}`}
+        >
+            <h1 className="text-[2em] font-semibold">Sign-in</h1>
+            {loginStatus.status !== 'loading' && (
+                <div className="pb-3">
+                    <div id="mojoauth-passwordless-form" className=""></div>
+                </div>
+            )}
+            {loginStatus.status === 'loading' && (
+                <div className="uppercase py-5">Processing Login</div>
+            )}
+            <button
+                disabled={loginStatus.status === 'loading'}
+                onClick={loginAnonymously}
+                className={`disabled:bg-slate-700 pb-[5px] bg-secondary text-white w-[370px] h-[50px] rounded-[10px] font-light ${centerStuffs}`}
+            >
+                Login Anonymously
+            </button>
+        </div>
+    );
+};
 
 export default Login;
