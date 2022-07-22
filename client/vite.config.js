b5aa978fabf10e4d4c48231eef8db668da36da59
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-07-22 02:23:04 +0000

    feat!: added prettier and eslint for proper & consistent coding style

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-	plugins: [react()],
+    plugins: [react(), eslint()],
 });
