37e2d4be761d802a13cb3f339836f18c6632e54f
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-02 05:59:35 +0100

    fix(NavBar.jsx): Confusing arrow function

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -11,42 +11,45 @@ const activeStyle = linkStyle + 'bg-primary shadow-2xl';
 const iconStyle = 'fill-[#f5f5f5] scale-[2]';
 
 const NavBar = () => {
-    return (
-        <div
-            className="
+
+
+
+  return (
+    <div
+      className="
     navContainer bg-secondary 
     w-[120px] min-h-[100vh]
     flex items-center flex-col
     justify-center shadow-[0_0_100px_0_rgba(0,0,0,1)] p-5"
+    >
+      <div className="justify-between flex items-center flex-col h-[35%] w-[100%]">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            (isActive ? activeStyle : linkStyle)
+          }
         >
-            <div className="justify-between flex items-center flex-col h-[35%] w-[100%]">
-                <NavLink
-                    to="/"
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : linkStyle
-                    }
-                >
-                    <RiUserSearchLine className={iconStyle} />
-                </NavLink>
-                <NavLink
-                    to="/friends"
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : linkStyle
-                    }
-                >
-                    <BsChatDots className={iconStyle} />
-                </NavLink>
-                <NavLink
-                    to="/profile"
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : linkStyle
-                    }
-                >
-                    <BiUserCircle className={iconStyle} />
-                </NavLink>
-            </div>
-        </div>
-    );
+          <RiUserSearchLine className={iconStyle} />
+        </NavLink>
+        <NavLink
+          to="/friends"
+          className={({ isActive }) =>
+            (isActive ? activeStyle : linkStyle)
+          }
+        >
+          <BsChatDots className={iconStyle} />
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            (isActive ? activeStyle : linkStyle)
+          }
+        >
+          <BiUserCircle className={iconStyle} />
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
