9de670c40cff99e80e8e56dcef8f420742ccb7a0
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-02 10:06:23 +0100

    refactor(app.jsx): remove console.log

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,38 +8,37 @@ import RoutesComponent from './components/Routes';
 import { useEffect } from 'react';
 
 function App() {
-    const dispatch = useDispatch();
-    const isLogged = useSelector((state) => state.isLogged);
-    const isUserLoggedIn = useSelector((state) => {
-        const { isLogged } = state;
+  const dispatch = useDispatch();
+  const isLogged = useSelector((state) => state.isLogged);
+  const isUserLoggedIn = useSelector((state) => {
+    const { isLogged } = state;
 
-        return isLogged.isLoggedIn && isLogged.loginId !== null;
-    });
+    return isLogged.isLoggedIn && isLogged.loginId !== null;
+  });
 
-    useEffect(() => {
-        // We need to convert the JSON string we saved to localStorage
-        // back to the original state object first before dispatching
-        // NOTE: Using try...catch here to prevent blank page on first visit
-        //       (previous issue that was solved)
-        try {
-            dispatch(changeIsLogged(JSON.parse(window.localStorage.isLogged)));
-        } catch {
-            dispatch(changeIsLogged({}));
-        }
-    }, []);
+  useEffect(() => {
+    // We need to convert the JSON string we saved to localStorage
+    // back to the original state object first before dispatching
+    // NOTE: Using try...catch here to prevent blank page on first visit
+    //       (previous issue that was solved)
+    try {
+      dispatch(changeIsLogged(JSON.parse(window.localStorage.isLogged)));
+    } catch {
+      dispatch(changeIsLogged({}));
+    }
+  }, []);
 
-    useEffect(() => {
-        console.log(isLogged);
-        // `localStorage` only accepts strings so have to convert the state to
-        // JSON format first
-        window.localStorage.setItem('isLogged', JSON.stringify(isLogged));
-    }, [isLogged]);
+  useEffect(() => {
+    // `localStorage` only accepts strings so have to convert the state to
+    // JSON format first
+    window.localStorage.setItem('isLogged', JSON.stringify(isLogged));
+  }, [isLogged]);
 
-    return (
-        <SocketContext.Provider value={socket}>
-            <div>{isUserLoggedIn ? <RoutesComponent /> : <Login />}</div>
-        </SocketContext.Provider>
-    );
+  return (
+    <SocketContext.Provider value={socket}>
+      <div>{isUserLoggedIn ? <RoutesComponent /> : <Login />}</div>
+    </SocketContext.Provider>
+  );
 }
 
 export default App;
