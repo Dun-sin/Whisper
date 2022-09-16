d04cf7628690ffccb7cbf468177d4e2ab9e7935f
Dunsin <jesudunsinfaiye@gmail.com>
2022-09-16 17:39:54 +0100

    fix: wrong direction to css file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import './assets/css/index.css'
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
