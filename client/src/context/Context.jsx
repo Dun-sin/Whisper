b6ece54ace96f2786f7796a00d48fed358c5b349
Mathias Ayivor <74247733+mathiasayivor@users.noreply.github.com>
2022-09-22 04:35:29 +0000

    feat: random pairing v2 (#84)
    * feat(backend): changed code to add chat resumability for re-connections
    
    * refactor(SocketContext): improved on socket.io initialization to provide typing support
    
    * feat(Searching Page):added chat restoration (from backend) after re-connections
    
    * fix: added id when sending message to backend
    
    * chore: added react-loading-icons library for loading animations
    
    * feat(Chat ux): added loading state + memory leak fix

--- client/src/context/Context.jsx
+++ client/src/context/Context.jsx
@@ -1,5 +1,5 @@
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
-export const socket = io.connect(import.meta.env.VITE_SOCKET_URL);
-export const SocketContext = createContext();
+export const socket = io(import.meta.env.VITE_SOCKET_URL);
+export const SocketContext = createContext(socket);
