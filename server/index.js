85f4199a2d3cec3023e1a10bd2ccc0fb4313522a
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-09-22 02:51:59 +0000

    feat(backend): changed code to add chat resumability for re-connections

--- server/index.js
+++ server/index.js
@@ -1,29 +1,40 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
-const cors = require("cors");
-const { Server } = require("socket.io");
+const cors = require('cors');
+const { Server } = require('socket.io');
 const io = new Server(server, {
-  cors: { origin: "*" },
-  reconnectionAttempts: 5,
+    cors: { origin: '*' },
+    reconnectionAttempts: 5,
 });
 const HTTP_PORT = process.env.PORT || 4000;
 
 // Mongodb database host connection
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MongoDB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 // User Schema
-const User = require("./models/UserSchema");
+const User = require('./models/UserSchema');
 
 // Modules
-const userModule = require("./users");
+const {
+    getWaitingUserLen,
+    getUser,
+    addUser,
+    addActiveUser,
+    addWaitingUser,
+    getUserRoom,
+    getRandomPairFromWaitingList,
+    isUserActive,
+    getActiveUser,
+    addToWaitingList,
+} = require('./users');
 
 app.use(express.json());
 app.use(cors());
@@ -32,100 +43,182 @@ app.use(cors());
   @method: post
   @end-point: /user/add
 */
-app.post("/user/add", (req, res) => {
-  User.create(
-    {
-      email: req.body.email,
-    },
-    (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.status(202).json(data);
-      }
-    }
-  );
+app.post('/user/add', (req, res) => {
+    User.create(
+        {
+            email: req.body.email,
+        },
+        (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.status(202).json(data);
+            }
+        }
+    );
 });
 
 /*
   @method: get
   @end-point: /user/find
 */
-app.get("/user/find", (req, res) => {
-  User.find(req.query, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (data.length === 0) {
-        res.sendStatus(202);
-      } else {
-        const user = {};
+app.get('/user/find', (req, res) => {
+    User.find(req.query, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (data.length === 0) {
+                res.sendStatus(202);
+            } else {
+                const user = {};
 
-        user["id"] = data[0]._id.toString();
-        res.status(200).send(JSON.stringify(user));
-      }
-    }
-  });
+                user['id'] = data[0]._id.toString();
+                res.status(200).send(JSON.stringify(user));
+            }
+        }
+    });
 });
 
-// this function will be triggred when ever the user from front-end will search for new user to chat.
-const matchMaker = () => {
-  // checking whether user is more than one in waiting list or not.
-  if (userModule.getWaitingUserLen() > 1) {
-    // getting user socket info form waiting user list and removing it from wating list.
-    let user1 = userModule.getUser();
-    let user2 = userModule.getUser();
-    // creating unique room for users.
-    let roomval = user1.id + user2.id;
-    // letting user to joing the room.
-    user1.join(roomval);
-    user2.join(roomval);
-    // sending message to users that they have joined the room.
-    io.to(roomval).emit("joined", "Searched completed");
-    // creating data structure to store user information in specific format.
-    let udata1 = {
-      id: user1.id,
-      room: roomval,
-    };
-    let udata2 = {
-      id: user2.id,
-      room: roomval,
-    };
-    // maintaining the user's room name
-    userModule.addUser(udata1);
-    userModule.addUser(udata2);
+/**
+ * this function will be triggred when ever the user from front-end will search
+ * for new user to chat.
+ *
+ * @param {Server} io
+ */
+const matchMaker = (io) => {
+    while (getWaitingUserLen() > 1) {
+        const pairedUsers = getRandomPairFromWaitingList();
 
-    // maintaining active user's list
-    userModule.addActiveUser({ id: udata1 });
-    userModule.addActiveUser({ id: udata1 });
-  }
+        const [user1, user2] = pairedUsers;
+
+        // TODO: Generate a unique roomId using uuid npm library
+        const newRoomId =
+            user1.emailOrLoginId.toString() + user2.emailOrLoginId.toString();
+        const chat = {
+            id: newRoomId,
+            userIds: [],
+            messages: [],
+            createdAt: new Date(),
+        };
+
+        pairedUsers.forEach((user) => {
+            chat.userIds.push(user.emailOrLoginId);
+            user.currentChatId = newRoomId;
+            user.chats[newRoomId] = chat;
+            user.socketConnections.map((socket) => {
+                socket.join(newRoomId);
+            });
+
+            addActiveUser(user);
+        });
+
+        io.to(newRoomId).emit('joined', {
+            roomId: newRoomId,
+        });
+    }
 };
 
 // Sockets
-io.on("connection", (socket) => {
-  socket.on("join", () => {
-    // whenever user is searching for another user for chat the user socket info will be added to user waiting list.
-    userModule.addWaitingUser(socket);
-    // if user is more than one in waiting list then it will create room for the two random users.
-    matchMaker();
-  });
+io.on('connection', (socket) => {
+    /**
+     * This event is emitted once the user clicks on the Start button or
+     * navigates to the /founduser route
+     */
+    socket.on('join', ({ loginId, email }) => {
+        /**
+         * This is necessary to enable us send notifications to users
+         * using multiple devices to chat
+         */
+        socket.join(loginId);
+        // Email is possibly null for anonymous users
+        if (email) {
+            socket.join(email);
+        }
 
-  socket.on("send_message", ({ senderId, message, time }, callback) => {
-    let room = userModule.getUserRoom(socket.id);
-    io.to(room).emit("receive_message", { senderId, message, time });
-  });
-  // socket.on('adding', (data) => {
-  // 	if (data.userID.ID === '') return;
-  // 	userModule.allUsers(data.userID.ID);
-  // });
+        /**
+         * First we check if user is already chatting.
+         * If user is already chatting, continue chat from where the user left
+         */
+        if (isUserActive(email ?? loginId)) {
+            const user = getActiveUser({
+                socketId: socket.id,
+                loginId,
+                email: email ?? null,
+            });
 
-  // socket.on('createRoom', () => {
-  // 	userModule.matchUsers(socket);
-  // });
+            // First join user to lost chat
+            socket.join(user.currentChatId);
+            user.socketConnections.push(socket);
+            user.socketIds.push(socket.id);
+
+            // Then return all chat messages
+            socket.emit('chat_restore', {
+                chats: user.chats,
+                currentChatId: user.currentChatId,
+            });
+            return;
+        }
+
+        // User was not having any previous chat. So add to waiting list
+        addToWaitingList({ loginId, email, socket });
+
+        // Finally, run matchMaker to pair all users on the waiting list
+        matchMaker(io);
+    });
+
+    socket.on('send_message', ({ senderId, id, message, time }, callback) => {
+        const user = getActiveUser({
+            socketId: socket.id,
+        });
+
+        if (!user) {
+            socket.emit('send_failed', {
+                message:
+                    'Hmmm. It seems your login session has expired. ' +
+                    'Re-login and try again',
+                messageId: id,
+            });
+        }
+
+        /**
+         * Cache the sent message for each user in the chat.
+         * This is also the point, where we persist the message in the db
+         */
+        user.chats[user.currentChatId ?? ''].userIds.forEach((userId) => {
+            const user = getActiveUser({
+                email: userId,
+                loginId: userId,
+            });
+
+            if (user) {
+                user.chats[user.currentChatId ?? ''].messages.push({
+                    id,
+                    message,
+                    time,
+                    senderId,
+                    type: 'message',
+                });
+            }
+        });
+
+        io.to(user.currentChatId).emit('receive_message', {
+            senderId,
+            message,
+            time,
+        });
+    });
+    // socket.on('adding', (data) => {
+    // 	if (data.userID.ID === '') return;
+    // 	userModule.allUsers(data.userID.ID);
+    // });
+
+    // socket.on('createRoom', () => {
+    // 	userModule.matchUsers(socket);
+    // });
 });
 
 app.use(cors());
 
 server.listen(HTTP_PORT, () => {
-  console.log(`on port ${HTTP_PORT}`);
+    console.log(`on port ${HTTP_PORT}`);
 });
