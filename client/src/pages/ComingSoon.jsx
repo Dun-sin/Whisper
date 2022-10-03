a8c5b446ade261746edae4ef88d8360874fb2958
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2022-10-03 13:44:03 +0530

    fix: make font-size responsive

--- client/src/pages/ComingSoon.jsx
+++ client/src/pages/ComingSoon.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ComingSoon = () => {
     return (
         <div
-            className={`bg-[#011627] min-w-[calc(100%-120px)] flex items-center justify-center flex-col max-h-[100vh] text-[#FFF] text-[1.5rem]`}
+            className={`bg-[#011627] min-w-[calc(100%-120px)] flex items-center justify-center flex-col max-h-[100vh] text-[#FFF] text-[calc(1.2vh+1.2vh+1.2vmin)]`}
         >
             Feature Coming Soon
         </div>
