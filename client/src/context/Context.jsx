a1b00d1bec86d9a7f02af3da76a8f2425b9e9ea4
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-10-14 13:51:06 +0000

    fix: delay in random pairing on initial load

--- client/src/context/Context.jsx
+++ client/src/context/Context.jsx
@@ -1,5 +1,8 @@
 import { io } from 'socket.io-client';
 import { createContext } from 'react';
 
-export const socket = io(import.meta.env.VITE_SOCKET_URL);
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
+    autoConnect: false
+});
+
 export const SocketContext = createContext(socket);
