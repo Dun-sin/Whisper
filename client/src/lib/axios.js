09ada827abebfee9f9395e6f41b4c83b5043fe19
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-10-16 12:20:49 +0000

    feat: created re-usable axios util

---
+++ client/src/lib/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_SOCKET_URL,
+    headers: {
+        Accept: 'application/json',
+    },
+});
+
+export { api };
