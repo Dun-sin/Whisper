e6763cca37b0f377fdea0cfb47821b00aa45fe12
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-10-16 11:59:09 +0000

    feat: improve on route guard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,39 @@
-import { SocketContext, socket } from './context/Context';
-
 // Components
 import Login from 'pages/Login';
-import RoutesComponent from 'components/Routes';
-import { useAuth } from './context/AuthContext';
+import { useAuth } from 'context/AuthContext';
+import NavBar from 'components/NavBar';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Start from 'pages/Start';
+import Searching from 'pages/Searching';
+import ComingSoon from 'pages/ComingSoon';
+import ProtectedRoutes from 'components/ProtectedRoutes';
 
 function App() {
     const { isLoggedIn } = useAuth();
 
     return (
-        <SocketContext.Provider value={socket}>
-            <div>{isLoggedIn ? <RoutesComponent /> : <Login />}</div>
-        </SocketContext.Provider>
+        <div className="flex">
+            {/* TODO: Create layouts */}
+            {isLoggedIn && <NavBar />}
+            <Routes>
+                <Route
+                    exact
+                    path="/"
+                    element={<ProtectedRoutes isLoggedIn={isLoggedIn} />}
+                >
+                    <Route exact path="/" element={<Start />} />
+                    <Route exact path="/founduser" element={<Searching />} />
+                    <Route exact path="/friends" element={<ComingSoon />} />
+                    <Route exact path="/profile" element={<ComingSoon />} />
+                </Route>
+
+                <Route
+                    exact
+                    path="/login"
+                    element={isLoggedIn ? <Navigate to="/" /> : <Login />}
+                />
+            </Routes>
+        </div>
     );
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'styles/index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import "rsuite/dist/rsuite.min.css";
+import 'rsuite/dist/rsuite.min.css';
+import { socket, SocketContext } from 'context/Context';
 
 render(
     <AuthProvider>
-            <Router>
+        <Router>
+            <SocketContext.Provider value={socket}>
                 <App />
-            </Router>
+            </SocketContext.Provider>
+        </Router>
     </AuthProvider>,
     document.getElementById('root')
 );
