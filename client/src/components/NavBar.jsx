eeb4ace0d5416576fa4bbe981461c80376b1f709
Mathias Ayivor <mathiasayivor5@gmail.com>
2022-10-22 21:32:13 +0000

    feat(NavBar.jsx): removed code duplication and added tooltip

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,13 +1,15 @@
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-// Icons
+import { Tooltip, Whisper } from 'rsuite';
 
+// Icons
 import { BsChatDots } from 'react-icons/bs';
 import { RiUserSearchLine } from 'react-icons/ri';
 import { BiUserCircle, BiLogOut } from 'react-icons/bi';
-import { useAuth } from 'context/AuthContext';
-import { SocketContext } from 'src/context/Context';
-import { useContext } from 'react';
 
+// Store
+import { useAuth } from 'context/AuthContext';
+import { SocketContext } from 'context/Context';
 
 const linkStyle = `h-[80px] w-[100%] flex items-center justify-center hover:bg-primary p-6 rounded-[15px] `;
 const activeStyle = linkStyle + 'bg-primary shadow-2xl';
@@ -21,42 +23,58 @@ const NavBar = () => {
         if (confirm('Are you sure you want to logout?')) {
             logout();
 
-            socket.emit('logout')
+            socket.emit('logout');
         }
     };
 
+    const getLinkStyle = ({ isActive }) => (isActive ? activeStyle : linkStyle);
+
     return (
-        <div
-            className="navContainer bg-secondary md:w-[120px] md:min-h-[100vh] mdl: flex items-center flex-col mdl:flex-row justify-center shadow-[0_0_100px_0_rgba(0,0,0,1)] p-5"
-        >
+        <div className="navContainer bg-secondary md:w-[120px] md:min-h-[100vh] mdl: flex items-center flex-col mdl:flex-row justify-center shadow-[0_0_100px_0_rgba(0,0,0,1)] p-5">
             <div className="justify-between flex items-center flex-col mdl:flex-row h-[35%] w-[100%] gap-3">
-                <NavLink
-                    to="/"
-                    className={({ isActive }) =>
-                        (isActive ? activeStyle : linkStyle)
-                    }
+                <Whisper
+                    placement="auto"
+                    controlId="control-id-hover"
+                    trigger="hover"
+                    speaker={<Tooltip>Search for random buddies</Tooltip>}
                 >
-                    <RiUserSearchLine className={iconStyle} />
-                </NavLink>
-                <NavLink
-                    to="/friends"
-                    className={({ isActive }) =>
-                        (isActive ? activeStyle : linkStyle)
-                    }
+                    <NavLink to="/" className={getLinkStyle}>
+                        <RiUserSearchLine className={iconStyle} />
+                    </NavLink>
+                </Whisper>
+                <Whisper
+                    placement="auto"
+                    controlId="control-id-hover"
+                    trigger="hover"
+                    speaker={<Tooltip>My Chats</Tooltip>}
                 >
-                    <BsChatDots className={iconStyle} />
-                </NavLink>
-                <NavLink
-                    to="/profile"
-                    className={({ isActive }) =>
-                        (isActive ? activeStyle : linkStyle)
-                    }
+                    <NavLink to="/friends" className={getLinkStyle}>
+                        <BsChatDots className={iconStyle} />
+                    </NavLink>
+                </Whisper>
+                <Whisper
+                    placement="auto"
+                    controlId="control-id-hover"
+                    trigger="hover"
+                    speaker={<Tooltip>My Profile</Tooltip>}
                 >
-                    <BiUserCircle className={iconStyle} />
-                </NavLink>
-                <button className={linkStyle} onClick={() => handleLogout()}>
-                    <BiLogOut className={iconStyle} />
-                </button>
+                    <NavLink to="/profile" className={getLinkStyle}>
+                        <BiUserCircle className={iconStyle} />
+                    </NavLink>
+                </Whisper>
+                <Whisper
+                    placement="auto"
+                    controlId="control-id-hover"
+                    trigger="hover"
+                    speaker={<Tooltip>Logout</Tooltip>}
+                >
+                    <button
+                        className={linkStyle}
+                        onClick={() => handleLogout()}
+                    >
+                        <BiLogOut className={iconStyle} />
+                    </button>
+                </Whisper>
             </div>
         </div>
     );
