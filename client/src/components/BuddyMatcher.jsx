98757b169469bc10a31dfaabc86e0a9cc3d0f359
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2022-10-26 20:46:19 +0100

    fix: missing dependency

--- client/src/components/BuddyMatcher.jsx
+++ client/src/components/BuddyMatcher.jsx
@@ -11,167 +11,167 @@ import { useApp } from 'src/context/AppContext';
 
 const stoppingSearchLoadingText = <p>Stopping the search</p>;
 const BuddyMatcher = () => {
-    const { playNotification } = useNotification();
-    const navigate = useNavigate();
-    const { auth } = useAuth();
-    const { createChat, closeChat, closeAllChats } = useChat();
-    const { startSearch, endSearch, app } = useApp();
+  const { playNotification } = useNotification();
+  const navigate = useNavigate();
+  const { auth } = useAuth();
+  const { createChat, closeChat, closeAllChats } = useChat();
+  const { startSearch, endSearch, app } = useApp();
 
-    const [isStoppingSearch, setIsStoppingSearch] = useState(false);
-    const socket = useContext(SocketContext);
+  const [isStoppingSearch, setIsStoppingSearch] = useState(false);
+  const socket = useContext(SocketContext);
 
-    const userID = auth.loginId;
-    const defaultLoadingText = <p>Looking for a random buddy</p>;
-    const [loadingText, setLoadingText] = useState(defaultLoadingText);
-    let timeout = null;
+  const userID = auth.loginId;
+  const defaultLoadingText = <p>Looking for a random buddy</p>;
+  const [loadingText, setLoadingText] = useState(defaultLoadingText);
+  let timeout = null;
 
-    const startNewSearch = () => {
-        startSearch();
-        setLoadingText(defaultLoadingText);
-        socket.emit('join', { loginId: auth.loginId, email: auth.email });
-    };
+  const startNewSearch = () => {
+    startSearch();
+    setLoadingText(defaultLoadingText);
+    socket.emit('join', { loginId: auth.loginId, email: auth.email });
+  };
 
-    const handleStopSearch = () => {
-        socket.emit('stop_search', {
-            loginId: auth.loginId,
-            email: auth.email,
-        });
-        setIsStoppingSearch(true);
-    };
+  const handleStopSearch = () => {
+    socket.emit('stop_search', {
+      loginId: auth.loginId,
+      email: auth.email,
+    });
+    setIsStoppingSearch(true);
+  };
 
-    useEffect(() => {
+  useEffect(() => {
 
-        setLoadingText(
-            isStoppingSearch ? stoppingSearchLoadingText : defaultLoadingText
-        );
-
-
-    useEffect(() => {
-        if (loadingText === defaultLoadingText) {
-            timeout = setTimeout(() => {
-                setLoadingText(
-                    <>
-                        <p>
-                            Taking too long? <br className="md:hidden" />
-                            No <span className="hidden sm:inline">
-                                chat
-                            </span>{' '}
-                            buddy is currently available :({' '}
-                        </p>
-                        <p>
-                            <a
-                                href="https://ctt.ac/US0h0"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                Tweet
-                            </a>{' '}
-                            about this app and get more people to use it!
-                        </p>
-                    </>
-                );
-            }, 15000);
-        }
-
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [loadingText]);
-
-    useEffect(() => {
-        if (!app.currentChatId) {
-            startSearch();
-        }
-
-        if (!socket.connected) {
-            socket.connect();
-        }
-
-        socket.on('close', (chatId) => {
-            endSearch();
-            closeChat(chatId);
-            playNotification('chatClosed');
-
-            if (
-                !confirm(
-                    'This chat is closed! Would you like to search for a new buddy?'
-                )
-            ) {
-                navigate('/');
-                return;
-            }
-
-            startNewSearch();
-        });
-
-        // This is necessary else chat won't be restored after re-connections
-        socket.on('connect', () => {
-            // Here server will be informed that user is searching for
-            // another user
-            socket.emit('join', { loginId: auth.loginId, email: auth.email });
-        });
-        socket.connected && socket.emit('adding', { userID });
-        socket.emit('createRoom', `${userID}-in-search`);
-        // From here will get the info from server that user has joined the room
-
-        socket.on('joined', ({ roomId, userIds }) => {
-            playNotification('buddyPaired');
-
-            createChat(roomId, userIds);
-            endSearch(roomId);
-        });
-
-        socket.on('chat_restore', ({ chats, currentChatId }) => {
-            Object.values(chats).forEach((chat) => {
-                createChat(
-                    chat.id,
-                    chat.userIds,
-                    chat.messages,
-                    chat.createdAt
-                );
-            });
-            endSearch(currentChatId);
-        });
-
-        socket.on('inactive', () => {
-            closeAllChats();
-        });
-
-        socket.on('stop_search_success', () => {
-            setIsStoppingSearch(false);
-            endSearch();
-            navigate('/');
-        });
-
-        return () => {
-            socket
-                .off('connect')
-                .off('joined')
-                .off('chat_restore')
-                .off('close')
-                .off('inactive');
-            socket.disconnect();
-        };
-    }, []);
-
-    return app.isSearching ? (
-        <div className="flex w-full justify-center items-center min-h-[calc(100vh-70px)] flex-col bg-primary">
-            <ThreeDots fill="rgb(255 159 28)" />
-            <div className="text-lg text-center text-white">{loadingText}</div>
-            {!isStoppingSearch && (
-                <button
-                    onClick={handleStopSearch}
-                    className={
-                        'hover:no-underline hover:text-white font-medium text-white text-[1.5em] w-[8em] h-[2.3em] mt-4 rounded-[30px] bg-[#FF3A46] flex flex-col items-center justify-center'
-                    }
-                >
-                    Stop
-                </button>
-            )}
-        </div>
-    ) : (
-        <Anonymous onChatClosed={startNewSearch} />
+    setLoadingText(
+      isStoppingSearch ? stoppingSearchLoadingText : defaultLoadingText
     );
+  }, [isStoppingSearch]);
+
+  useEffect(() => {
+    if (loadingText === defaultLoadingText) {
+      timeout = setTimeout(() => {
+        setLoadingText(
+          <>
+            <p>
+              Taking too long? <br className="md:hidden" />
+              No <span className="hidden sm:inline">
+                chat
+              </span>{' '}
+              buddy is currently available :({' '}
+            </p>
+            <p>
+              <a
+                href="https://ctt.ac/US0h0"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Tweet
+              </a>{' '}
+              about this app and get more people to use it!
+            </p>
+          </>
+        );
+      }, 15000);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [loadingText]);
+
+  useEffect(() => {
+    if (!app.currentChatId) {
+      startSearch();
+    }
+
+    if (!socket.connected) {
+      socket.connect();
+    }
+
+    socket.on('close', (chatId) => {
+      endSearch();
+      closeChat(chatId);
+      playNotification('chatClosed');
+
+      if (
+        !confirm(
+          'This chat is closed! Would you like to search for a new buddy?'
+        )
+      ) {
+        navigate('/');
+        return;
+      }
+
+      startNewSearch();
+    });
+
+    // This is necessary else chat won't be restored after re-connections
+    socket.on('connect', () => {
+      // Here server will be informed that user is searching for
+      // another user
+      socket.emit('join', { loginId: auth.loginId, email: auth.email });
+    });
+    socket.connected && socket.emit('adding', { userID });
+    socket.emit('createRoom', `${userID}-in-search`);
+    // From here will get the info from server that user has joined the room
+
+    socket.on('joined', ({ roomId, userIds }) => {
+      playNotification('buddyPaired');
+
+      createChat(roomId, userIds);
+      endSearch(roomId);
+    });
+
+    socket.on('chat_restore', ({ chats, currentChatId }) => {
+      Object.values(chats).forEach((chat) => {
+        createChat(
+          chat.id,
+          chat.userIds,
+          chat.messages,
+          chat.createdAt
+        );
+      });
+      endSearch(currentChatId);
+    });
+
+    socket.on('inactive', () => {
+      closeAllChats();
+    });
+
+    socket.on('stop_search_success', () => {
+      setIsStoppingSearch(false);
+      endSearch();
+      navigate('/');
+    });
+
+    return () => {
+      socket
+        .off('connect')
+        .off('joined')
+        .off('chat_restore')
+        .off('close')
+        .off('inactive');
+      socket.disconnect();
+    };
+  }, []);
+
+  return app.isSearching ? (
+    <div className="flex w-full justify-center items-center min-h-[calc(100vh-70px)] flex-col bg-primary">
+      <ThreeDots fill="rgb(255 159 28)" />
+      <div className="text-lg text-center text-white">{loadingText}</div>
+      {!isStoppingSearch && (
+        <button
+          onClick={handleStopSearch}
+          className={
+            'hover:no-underline hover:text-white font-medium text-white text-[1.5em] w-[8em] h-[2.3em] mt-4 rounded-[30px] bg-[#FF3A46] flex flex-col items-center justify-center'
+          }
+        >
+          Stop
+        </button>
+      )}
+    </div>
+  ) : (
+    <Anonymous onChatClosed={startNewSearch} />
+  );
 };
 
 export default BuddyMatcher;
