72e7d2368a95bdc3e0facca42eb788b8d2bfd001
Dunsin <jesudunsinfaiye@gmail.com>
2023-09-12 19:40:09 +0100

    refactor: use react 18 create root

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'styles/index.css';
 import App from './App';
@@ -10,18 +10,17 @@ import { socket, SocketContext } from 'context/Context';
 import { DialogProvider } from './context/DialogContext';
 import Dialog from './components/Dialog';
 
-render(
-    <AuthProvider>
-        <AppProvider>
-            <Router>
-                <SocketContext.Provider value={socket}>
-                    <DialogProvider>
-                        <App />
-                        <Dialog />
-                    </DialogProvider>
-                </SocketContext.Provider>
-            </Router>
-        </AppProvider>
-    </AuthProvider>,
-    document.getElementById('root')
-);
+
+const root = createRoot(document.getElementById('root'))
+root.render(<AuthProvider>
+    <AppProvider>
+        <Router>
+            <SocketContext.Provider value={socket}>
+                <DialogProvider>
+                    <App />
+                    <Dialog />
+                </DialogProvider>
+            </SocketContext.Provider>
+        </Router>
+    </AppProvider>
+</AuthProvider>)
