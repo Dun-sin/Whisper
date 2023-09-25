60dfacfbe5f47644355c2bf2b6c1878654c7aff4
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2023-09-25 19:53:30 +0100

    fix: editing returning an html element (#282)

---
+++ client/src/lib/badWords.js
@@ -0,0 +1,17 @@
+const listOfBadWordsNotAllowed = [
+    'sex',
+    'porn',
+    'fuck',
+    'cock',
+    'titties',
+    'boner',
+    'muff',
+    'pussy',
+    'asshole',
+    'cunt',
+    'ass',
+    'cockfoam',
+    'nigger',
+];
+
+export default listOfBadWordsNotAllowed;
