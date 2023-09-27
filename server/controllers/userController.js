0e18d4b1a50ae9e549b7224292e611b98c2f1f57
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2023-09-27 22:00:49 +0100

    feat: add user profile  (#315)
    Co-authored-by: Stephen <105071093+stephenokyere233@users.noreply.github.com>
    Co-authored-by: Victor Fedorov <vpfedorov25@gmail.com>
    Co-authored-by: Anubhav Adhikari <53504460+anubhav1206@users.noreply.github.com>
    Co-authored-by: Ishan Siddiqui <77967215+ishan-siddiqui@users.noreply.github.com>

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,38 +37,91 @@ const loginUser = async (req, res) => {
 	}
 };
 
-const addUser = (req, res) => {
-	User.create(
-		{
-			email: req.body.email,
-		},
-		(err, data) => {
-			if (err) {
-				return err;
-			}
-			return res.status(202).json(data);
-		},
-	);
+const getProfile = async (req, res) => {
+	try {
+		const { email } = req.params;
+
+		// Find the user by email
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(404).json({ error: 'User not found' });
+		}
+
+		// Send the user profile data as JSON response
+		res.status(200).json(user);
+	} catch (error) {
+		console.error('Error fetching user profile:', error);
+		res.status(500).json({ error: 'Internal server error' });
+	}
 };
 
-const findUser = (req, res) => {
-	User.find(req.query, (err, data) => {
-		if (err) {
-			return err;
-		} else {
-			if (data.length === 0) {
-				return res.sendStatus(202);
-			} else {
-				const user = {};
+const updateProfile = async (req, res) => {
+	const { username, aboutMe, gender, age, email } = req.body;
 
-				user['id'] = data[0]._id.toString();
-				return res.status(200).send(JSON.stringify(user));
-			}
+	if (typeof email !== 'string' || !validator.isEmail(email)) {
+		res.status(406).json({
+			message: 'Email is invalid',
+		});
+
+		return;
+	}
+
+	try {
+		// Find the user by email
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(404).json({ error: 'User not found' });
 		}
-	});
+
+		// Update user's profile with provided fields or defaults
+		user.username = username || 'Anonymous';
+		user.aboutMe = aboutMe || null;
+		user.gender = gender || 'Unknown';
+		user.age = age || null;
+
+		// Save the updated user profile
+		await user.save();
+
+		return res.status(200).json({ message: 'Profile updated successfully' });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ error: 'Internal server error' });
+	}
+};
+
+const deleteUser = async (req, res) => {
+	const { email } = req.body;
+
+	if (typeof email !== 'string' || !validator.isEmail(email)) {
+		res.status(406).json({
+			message: 'Email is invalid',
+		});
+
+		return;
+	}
+	try {
+		// Find the user by email
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(404).json({ error: 'User not found' });
+		}
+
+		// Delete the user
+		await user.deleteOne();
+
+		return res.status(200).json({ message: 'User deleted successfully' });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ error: 'Internal server error' });
+	}
 };
 
 UserRouter.route('/login').post(loginUser);
-UserRouter.route('/user').post(addUser).get(findUser);
+UserRouter.route('/profile').post(updateProfile);
+UserRouter.route('/profile/:email').get(getProfile);
+UserRouter.route('/deleteUser').delete(deleteUser);
 
 module.exports = UserRouter;
