9a7e5798872f69e5215b1a1107d53e41a105dcef
Dudu Popkhadze <dudupopkhadze@gmail.com>
2023-10-04 17:06:42 +0400

    fix:searching-ui-flickering

--- client/src/components/BuddyMatcher.jsx
+++ client/src/components/BuddyMatcher.jsx
@@ -155,7 +155,7 @@ const BuddyMatcher = () => {
         };
     }, []);
 
-    return app.isSearching ? (
+    return app.isSearching || !app.currentChatId ? (
         <div className="flex w-full justify-center items-center min-h-[calc(100vh-70px)] flex-col bg-primary">
             <ThreeDots fill="rgb(255 159 28)" />
             <div className="text-lg text-center text-white">{loadingText}</div>
