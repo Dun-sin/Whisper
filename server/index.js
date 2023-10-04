a5f67a103bda5aa2754cd75e1d94631d5d705854
Ayush Kumar Pandit <31253617+Ayushpanditmoto@users.noreply.github.com>
2023-10-04 15:26:47 +0530

    fix: Mongodb path issues not able to detect .env file solved

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env_sample") });
 
 const express = require('express');
 const app = express();
