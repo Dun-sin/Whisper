f791968a739d5b0897161a207cc5132dbcc9a113
Mathias Ayivor <74247733+mathiasayivor@users.noreply.github.com>
2023-10-06 04:55:33 +0000

    feat: inform user for lack of connection (#324)
    Co-authored-by: Dunsin <78784850+Dun-sin@users.noreply.github.com>

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -63,7 +63,7 @@ const NavBar = ({ className }) => {
     const fullscreenPages = ['/founduser'];
 
     const hideNavbar = useMemo(
-        () => fullscreenPages.includes(location.pathname) && !app.isSearching,
+        () => fullscreenPages.includes(location.pathname) && app.currentChatId,
         [location, app]
     );
 
