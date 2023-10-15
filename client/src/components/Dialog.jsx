f5191c5003860b5a40fe360ceda8f9c165988238
Ahmed BENTAJ HAMYANI <ahmedbentaj710@gmail.com>
2023-10-15 12:07:06 +0100

    feat: add light mode (#392)
    Co-authored-by: Dunsin <78784850+Dun-sin@users.noreply.github.com>

--- client/src/components/Dialog.jsx
+++ client/src/components/Dialog.jsx
@@ -4,10 +4,13 @@ import PropTypes from 'prop-types';
 import { useDialog } from 'src/context/DialogContext';
 
 import 'styles/dialog.css';
+import { useDarkMode } from 'src/context/DarkModeContext';
 
 const Dialog = ({ ...rest }) => {
     const { dialog, setDialog } = useDialog();
     const { isOpen, text, handler, noBtnText, yesBtnText } = dialog;
+
+    const { darkMode } = useDarkMode();
 
     const resetDialog = () => {
         setDialog({ isOpen: false, text: '', handler: null });
@@ -37,20 +40,20 @@ const Dialog = ({ ...rest }) => {
     }
 
     return ReactDOM.createPortal(
-        <>
+        <div className={`${darkMode && 'dark'}`}>
             <div
                 id="overlay"
                 className="dialog-overlay"
                 onClick={resetDialog}
             />
             <dialog open={isOpen} {...rest} className="dialog-container">
-                <p className="text-white text-[1rem]">
+                <p className="text-primary dark:text-white text-[1rem]">
                     {text || 'Are you sure?'}
                 </p>
 
                 <div className="flex justify-end gap-2 mt-6">
                     <button
-                        className="dialog-btn bg-primary"
+                        className="dialog-btn bg-gray-100 dark:bg-primary text-primary dark:text-white"
                         onClick={handleNoClick}
                     >
                         {noBtnText || 'Cancel'}
@@ -63,7 +66,7 @@ const Dialog = ({ ...rest }) => {
                     </button>
                 </div>
             </dialog>
-        </>,
+        </div>,
         document.getElementById('portal')
     );
 };
--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -5,13 +5,24 @@ import {
     ButtonToolbar,
     Divider,
     Form,
+// <<<<<<< feature/light-mode
+    Dropdown,
+    Message,
     Slider,
     Toggle
 } from 'rsuite';
+// <<<<<<< feature/light-mode
+
 import { Icon } from '@iconify/react';
+
+// =======
+// import { Icon } from '@iconify/react';
+// >>>>>>> light-mode
 import { useApp } from 'src/context/AppContext';
 import { useAuth } from 'src/context/AuthContext';
 import { api } from 'src/lib/axios';
+
+import { useDarkMode } from 'src/context/DarkModeContext';
 
 const Searching = () => {
     const {
@@ -55,10 +66,111 @@ const Searching = () => {
         updateTmpSettings(newSettings);
     };
 
+    const { darkMode, setDarkMode } = useDarkMode();
+
     return (
         <div
-            className="flex justify-center items-center flex-col min-w-[calc(100%-120px)] p-2 gap-5 bg-primary min-h-full"
+            className="flex justify-center items-center flex-col min-w-[calc(100%-120px)] p-2 gap-5 bg-light dark:bg-primary min-h-full"
         >
+// <<<<<<< feature/light-mode
+//             <Form onSubmit={handleSubmit} onChange={handleChange}>
+//                 <Divider className="text-primary dark:text-white">Mode</Divider>
+//                 <ButtonToolbar>
+//                     <Dropdown
+//                         active
+//                         appearance='primary'
+//                         title={darkMode ?
+//                             <div className='flex items-center'>
+//                                 <Icon
+//                                     className='mr-2'
+//                                     icon="ic:round-dark-mode"
+//                                     color="primary"
+//                                     height="14"
+//                                     width="14"
+//                                 />
+//                                 <p>Dark</p>
+//                             </div>
+//                             :
+//                             <div className='flex items-center'>
+//                                 <Icon
+//                                     className='mr-2'
+//                                     icon="entypo:light-up"
+//                                     color="primary"
+//                                     height="14"
+//                                     width="14"
+//                                 />
+//                                 <p>Light</p>
+//                             </div>
+//                         }
+//                         activeKey={darkMode ? "dark" : "light"}
+//                         onSelect={(eventKey) => {
+//                             setDarkMode(
+//                                 eventKey === "dark" ? true : false
+//                             )
+//                         }}
+//                     >
+//                         <Dropdown.Item
+//                             eventKey="dark"
+//                             icon={<Icon
+//                                 icon="ic:round-dark-mode"
+//                                 color="primary"
+//                                 height="14"
+//                                 width="14"
+//                             />}
+//                             className='grid grid-cols-2 items-center pr-10'
+//                         >
+//                             Dark
+//                         </Dropdown.Item>
+//                         <Dropdown.Item
+//                             eventKey="light"
+//                             icon={<Icon
+//                                 className='mr-2'
+//                                 icon="entypo:light-up"
+//                                 color="primary"
+//                                 height="14"
+//                                 width="14"
+//                             />}
+//                             className='grid grid-cols-2 items-center pr-10'
+//                         >
+//                             Light
+//                         </Dropdown.Item>
+//                     </Dropdown>
+//                 </ButtonToolbar>
+
+//                 <Divider className="text-primary dark:text-white">Notifications</Divider>
+//                 <Form.Group controlId="notifications-enabled">
+//                     <Form.Control
+//                         name="notificationsEnabled"
+//                         accepter={Checkbox}
+//                         checked={settings.notificationsEnabled}
+//                         value={!settings.notificationsEnabled}
+//                     >
+//                         Enable Notifications
+//                     </Form.Control>
+//                     <Form.HelpText tooltip>
+//                         Enable/Disable notifications
+//                     </Form.HelpText>
+//                 </Form.Group>
+//                 <Form.Group controlId="notification-volume">
+//                     <Form.ControlLabel>Volume level (%)</Form.ControlLabel>
+//                     <Form.Control
+//                         name="notificationVolume"
+//                         accepter={Slider}
+//                         progress
+//                         step={10}
+//                         graduated
+//                         renderMark={(mark) => mark}
+//                         value={settings.notificationVolume}
+//                         disabled={!settings.notificationsEnabled}
+//                     />
+//                     <Form.HelpText tooltip>
+//                         Set the volume level for all your notifications
+//                     </Form.HelpText>
+//                 </Form.Group>
+//                 <Form.Group>
+//                     <ButtonToolbar className="flex justify-end">
+//                         <Animation.Fade in={hasUnsavedSettings}>
+// =======
             <div className='w-[33%] h-[85%] bg-secondary bg-opacity-30 rounded-3xl px-14 py-20 border border-slate-800'>
                 <Form onSubmit={handleSubmit} onChange={handleChange}>
                     <Form.Group className='flex justify-between' controlId="notifications-enabled">
@@ -179,18 +291,45 @@ const Searching = () => {
                                     Cancel
                                 </Button>
                             </Animation.Fade>
-                            <Button
-                                type="submit"
-                                appearance="primary"
-                                disabled={!hasUnsavedSettings}
-                                className='text-base font-normal w-24 h-9 bg-blue-500 rounded-md hover:bg-blue-400'
-                            >
-                                Update
-                            </Button>
+// >>>>>>> light-mode
+//                             <Button
+//                                 type="submit"
+//                                 appearance="primary"
+//                                 disabled={!hasUnsavedSettings}
+//                                 className='text-base font-normal w-24 h-9 bg-blue-500 rounded-md hover:bg-blue-400'
+//                             >
+//                                 Update
+//                             </Button>
+// <<<<<<< feature/light-mode
+//                         </Animation.Fade>
+//                         <Button
+//                             type="submit"
+//                             appearance="primary"
+//                             active
+//                             disabled={!hasUnsavedSettings}
+//                         >
+//                             Update Settings
+//                         </Button>
+//                     </ButtonToolbar>
+//                 </Form.Group>
+
+//                 <Animation.Bounce in={hasUnsavedSettings}>
+//                     <Message
+//                         showIcon
+//                         type="warning"
+//                         header="Warning!"
+//                         className="w-full"
+//                     >
+//                         You have unsaved settings!
+//                     </Message>
+//                 </Animation.Bounce>
+//             </Form>
+// =======
                         </ButtonToolbar>
                     </Form.Group>
                 </Form>
             </div>
+// >>>>>>> light-mode
         </div>
     );
 };
