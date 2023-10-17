aec5fe0334fe6d31069613409d586c461855570e
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2023-10-17 08:11:09 +0100

    refactor: remove apply for styling (#453)

--- client/src/components/Dialog.jsx
+++ client/src/components/Dialog.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { useDialog } from 'src/context/DialogContext';
 
-import 'styles/dialog.css';
 import { useDarkMode } from 'src/context/DarkModeContext';
 
 const Dialog = ({ ...rest }) => {
@@ -43,23 +42,23 @@ const Dialog = ({ ...rest }) => {
         <div className={`${darkMode && 'dark'}`}>
             <div
                 id="overlay"
-                className="dialog-overlay"
+                className="z-50 fixed inset-0 bg-black opacity-60"
                 onClick={resetDialog}
             />
-            <dialog open={isOpen} {...rest} className="dialog-container">
+            <dialog open={isOpen} {...rest} className="z-50 rounded-lg fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 m-0 bg-light dark:bg-secondary w-[90%] md:w-auto md:max-w-md px-6 py-6">
                 <p className="text-primary dark:text-white text-[1rem]">
                     {text || 'Are you sure?'}
                 </p>
 
                 <div className="flex justify-end gap-2 mt-6">
                     <button
-                        className="dialog-btn bg-gray-100 dark:bg-primary text-primary dark:text-white"
+                        className="hover:opacity-80 text-white px-4 py-2 rounded-lg bg-gray-100 dark:bg-primary text-primary dark:text-white"
                         onClick={handleNoClick}
                     >
                         {noBtnText || 'Cancel'}
                     </button>
                     <button
-                        className="dialog-btn bg-[#FF9F1C]"
+                        className="hover:opacity-80 text-white px-4 py-2 rounded-lg bg-[#FF9F1C]"
                         onClick={handleYesClick}
                     >
                         {yesBtnText || 'Confirm'}
