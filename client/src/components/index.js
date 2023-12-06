53f152d5c16d5a305b312d8477cc6d8522549f2f
Gomohit <99570471+Gomohit@users.noreply.github.com>
2023-12-06 12:22:28 +0000

    index file

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import Anonymous from "./Anonymous";
+import BuddyMatcher from "./BuddyMatcher";
+import Chat from "./Chat";
+import Dialog from "./Dialog";
+import EmojiPicker from "./EmojiPicker";
+import MessageStatus from "./MessageStatus";
+import NavBar from "./NavBar";
+import ProtectedRoutes from "./ProtectedRoutes";
+import UserAvatar from "./UserAvatar";
+
+export {Anonymous,BuddyMatcher,Chat,Dialog,EmojiPicker,MessageStatus,NavBar,ProtectedRoutes,UserAvatar}
---
+++ client/src/pages/index.js
@@ -0,0 +1,10 @@
+import ComingSoon from "./ComingSoon";
+import Freinds from   "./Friends"
+import Login from   "./Login"
+import NoPage from   "./NoPage"
+import Profile from   "./Profile"
+import Searching from   "./Searching"
+import SignupAnonUser from   "./SignupAnonUser"
+import Start from   "./Start"
+
+export {ComingSoon,Freinds,Login,NoPage,Profile,Searching,SignupAnonUser,Start}
