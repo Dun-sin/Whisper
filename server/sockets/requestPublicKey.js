9a6d5c20fb602680b8dee7bb802ab7fadecf63da
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2024-06-23 12:09:13 +0100

    fix: missing closing tag

--- server/sockets/requestPublicKey.js
+++ server/sockets/requestPublicKey.js
@@ -5,4 +5,5 @@ module.exports = (io, socket) => {
   socket.on(NEW_EVENT_REQUEST_PUBLIC_KEY, ({ chatId, publicKey, privateKey }) => {
     const keys = { pemPublicKeyString: publicKey, pemPrivateKeyString: privateKey };
     socket.broadcast.to(chatId).emit('publicKey', keys);
-});
+  })
+}
