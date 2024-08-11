6cda37a8d259d9b45fdc06936739623050d07a7a
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2024-08-11 12:19:48 +0100

    Apply suggestions from code review

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -210,7 +210,7 @@ const Profile = () => {
 										placeholder="19"
 										className="outline-none bg-transparent text-right"
 										ref={ageRef}
-										min="1"
+										min="12"
 										onChange={(e) => {
 											if (e.target.value < 12) {
 												e.target.value = '12';
