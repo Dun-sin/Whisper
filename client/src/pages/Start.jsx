cce4e79be3bf7c6cc10910b813f601520d7706a8
Priyanshu-2005 <pr.agarwal2005@gmail.com>
2024-10-07 21:44:29 +0530

    Close chat functionality in home screen

--- client/src/pages/Start.jsx
+++ client/src/pages/Start.jsx
@@ -1,10 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+import { NEW_EVENT_CLOSE, NEW_EVENT_INACTIVE, NEW_EVENT_JOIN } from '../../../constants.json';
 import { RiUserSearchLine } from 'react-icons/ri';
-import { useApp } from 'src/context/AppContext';
 
+// Store
+import { connectWithId, socket } from 'src/lib/socketConnection';
 import { createClassesFromArray } from 'src/lib/utils';
+import { useApp } from 'src/context/AppContext';
+import { useNotification } from 'src/lib/notification';
+
+import { useAuth } from 'src/context/AuthContext';
+import { useChat } from 'src/context/ChatContext';
+import useCheckTimePassed from 'src/hooks/useCheckTimePassed';
 
 import { requestBrowserNotificationPermissions } from 'src/lib/browserNotification';
 
@@ -12,8 +20,9 @@ const centerElement = 'flex flex-col items-center justify-center';
 
 const Start = () => {
 	const { app } = useApp();
+	const { clearTimer } = useCheckTimePassed();
 	const navigate = useNavigate();
-
+	const { playNotification } = useNotification();
 	useEffect(() => {
 		if (app.isSearching) {
 			navigate('/founduser');
@@ -21,6 +30,76 @@ const Start = () => {
 
 		requestBrowserNotificationPermissions();
 	}, []);
+	const { authState } = useAuth();
+	const { closeChat } = useChat();
+
+	const onClose = useCallback((chatId) => {
+		closeChat(chatId);
+		playNotification('chatClosed');
+
+		if (!confirm('This chat is closed! Would you like to search for a new buddy?')) {
+			navigate('/');
+			return;
+		}
+	}, []);
+	const onConnect = useCallback(() => {
+		socket.emit(NEW_EVENT_JOIN, {
+			loginId: authState.loginId,
+			email: authState.email,
+		});
+		console.log('chat socket connected');
+	}, []);
+
+	const onInactive = useCallback(() => {
+		closeChat(app.currentChatId);
+	}, []);
+
+	const emitClose = useCallback((err, chatClosed) => {
+		console.log(err, chatClosed);
+		if (err) {
+			alert('An error occured whiles closing chat.');
+			console.log(err);
+			return err;
+		}
+
+		if (chatClosed) {
+			closeChat(app.currentChatId);
+			app.currentChatId = null;
+		} else {
+			navigate('/');
+		}
+
+		clearTimer();
+	}, []);
+	useEffect(() => {
+		console.log('is socket connected : ', socket.connected);
+		if (!socket.connected) {
+			connectWithId(app.currentChatId);
+		}
+		socket.on('connect', onConnect);
+
+		socket.on(NEW_EVENT_CLOSE, onClose);
+
+		socket.on(NEW_EVENT_INACTIVE, onInactive);
+		return () => {
+			socket.off('connect', onConnect);
+			socket.off(NEW_EVENT_CLOSE, onClose);
+			socket.off(NEW_EVENT_INACTIVE, onInactive);
+			socket.disconnect();
+			console.log('useEffect used');
+		};
+	}, []);
+
+	const doCloseChat = () => {
+		console.log('ChatID', app.currentChatId);
+		console.log('is socket connected for closure ', socket.connected, socket.id);
+
+		if (!socket.connected) {
+			connectWithId(app.currentChatId);
+		}
+		socket.timeout(30000).emit(NEW_EVENT_CLOSE, app.currentChatId, emitClose);
+	};
+
 	return (
 		<div
 			className={createClassesFromArray([
@@ -56,6 +135,32 @@ const Start = () => {
 			>
 				{app.currentChatId ? 'Open Chat' : 'Start'}
 			</Link>
+			{app.currentChatId ? (
+				<button
+					className={createClassesFromArray([
+						centerElement,
+						'hover:no-underline',
+						'hover:text-black',
+						'font-medium',
+						'text-black',
+						'text-[1.5em]',
+						'w-[8em]',
+						'h-[2.3em]',
+
+						'mt-[5px]',
+						'rounded-[30px]',
+						'transition duration-300 ease-in-out',
+						'bg-[#EF4444] hover:bg-[#F87171]',
+					])}
+					onClick={() => {
+						doCloseChat();
+					}}
+				>
+					Close Chat
+				</button>
+			) : (
+				''
+			)}
 		</div>
 	);
 };
